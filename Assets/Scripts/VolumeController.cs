4dec5464e303dec8e895852f6fd25ac19285e19b
kalinswartz <kalinswartz@gmail.com>
2023-10-08 17:48:04 -0500

    Volume controller update and music added

--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -6,32 +6,36 @@ using Vuforia;
 
 public class VolumeController : MonoBehaviour
 {
+    private AudioSource backgroundMusic;
+    Vector3 startingRotation;
+    Vector3 currentRotation;
 
-    [SerializeField] AudioSource backgroundMusic;
-    Quaternion startingRotation;
     // Start is called before the first frame update
     void Start()
     {
+        backgroundMusic = GetComponent<AudioSource>();
         backgroundMusic.enabled = true;
-        backgroundMusic.volume = 1;
-        startingRotation = transform.rotation;
+        backgroundMusic.volume = 0.5f;
+        startingRotation = transform.eulerAngles;
         Debug.Log(startingRotation);
     }
     
     // Update is called once per frame
     void Update()
     {
-        if (transform.rotation.x > startingRotation.x)
+        currentRotation = transform.eulerAngles;
+        //Debug.Log(currentRotation);
+        if (currentRotation.x < startingRotation.x)
         {
             increaseVolume();
         }
-        else if (transform.rotation.x < startingRotation.x)
+        else if (currentRotation.x > startingRotation.x)
         {
             decreaseVolume();
         } 
         else //if rotation is == startingRotation
         {
-            backgroundMusic.volume = 1;
+            backgroundMusic.volume = 0.5f;
         }
     }
 
@@ -39,7 +43,7 @@ public class VolumeController : MonoBehaviour
     {
         if (backgroundMusic.volume < 1)
         {
-            backgroundMusic.volume++;
+            backgroundMusic.volume += 0.01f;
         }
 
     }
@@ -48,7 +52,7 @@ public class VolumeController : MonoBehaviour
     {
         if (backgroundMusic.volume > 0)
         {
-            backgroundMusic.volume--;
+            backgroundMusic.volume -= 0.01f;
         }
         
     }
